3346b64fc9b8c996083a5511d21ec1df84ab55d7
Catalina Nuñez <catalina@example.com>
2025-03-21 15:12:00 -0500

    Se agregan operaciones como la exponenciación y módulo al js

--- calculadora.js
+++ calculadora.js
@@ -16,7 +16,20 @@ function calcular(){
         } else {
             resultado = resultado + (valor1 / valor2)
         }  
-    } else {
+    } else if(operador === "**"){
+        if (valor1 === 0 && valor2 === 0){
+            resultado = "Indeterminado cuando la base y el exponente equivalen a 0"
+        } else {
+            resultado = resultado + (valor1 ** valor2)
+        }
+    } else if (operador === "%"){
+        if (valor2 === 0){
+            resultado = "¡No se puede dividir por cero!"
+        } else {
+            resultado = resultado + (valor1 % valor2)
+        }  
+    }
+    else {
         resultado = "ERROR: Operador No Válido!"
     }
     document.getElementById("resultado").innerText = resultado
